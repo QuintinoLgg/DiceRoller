a4347a8b7507c6990cb89b70b6b0e9de3a0e4b59
quintinolgg <s20120151@alumnos.itsur.edu.mx>
2023-09-23 00:34:53 -0600

    Final

---
+++ SuperHeroe/app/src/main/java/model/ObjectHeroe.kt
@@ -0,0 +1,38 @@
+package model
+
+import com.example.superheroe.R
+
+object ObjectHeroe {
+    val heroes = listOf(
+        ClassHeroe(
+            nameRes = R.string.hero1,
+            descriptionRes = R.string.description1,
+            imageRes = R.drawable.android_superhero1
+        ),
+        ClassHeroe(
+            nameRes = R.string.hero2,
+            descriptionRes = R.string.description2,
+            imageRes = R.drawable.android_superhero2
+        ),
+        ClassHeroe(
+            nameRes = R.string.hero3,
+            descriptionRes = R.string.description3,
+            imageRes = R.drawable.android_superhero3
+        ),
+        ClassHeroe(
+            nameRes = R.string.hero4,
+            descriptionRes = R.string.description4,
+            imageRes = R.drawable.android_superhero4
+        ),
+        ClassHeroe(
+            nameRes = R.string.hero5,
+            descriptionRes = R.string.description5,
+            imageRes = R.drawable.android_superhero5
+        ),
+        ClassHeroe(
+            nameRes = R.string.hero6,
+            descriptionRes = R.string.description6,
+            imageRes = R.drawable.android_superhero6
+        )
+    )
+}
